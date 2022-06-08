1012140c5779e29649fec897c85b09017c304513
Elmer Adrián Ortega Valdés <elmeradrianv@fb.com>
2022-06-08 11:01:45 -0700

    Step 8: Add floating action button to dial the contact's number

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,7 +1,9 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +20,7 @@ import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.codepath.android.lollipopexercise.R;
 import com.codepath.android.lollipopexercise.models.Contact;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DetailsActivity extends AppCompatActivity {
     public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
@@ -26,6 +29,7 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView tvName;
     private TextView tvPhone;
     private View vPalette;
+    private FloatingActionButton fabCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,7 @@ public class DetailsActivity extends AppCompatActivity {
         tvName = (TextView) findViewById(R.id.tvName);
         tvPhone = (TextView) findViewById(R.id.tvPhone);
         vPalette = findViewById(R.id.vPalette);
-
+        fabCall=findViewById(R.id.fabCall);
         // Extract contact from bundle
         mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
 
@@ -69,6 +73,18 @@ public class DetailsActivity extends AppCompatActivity {
 
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
+
+        // Dial contact's number.
+        // This shows the dialer with the number, allowing you to explicitly initiate the call.
+        fabCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
 
     }
 
